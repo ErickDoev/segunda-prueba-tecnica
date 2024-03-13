d688e362492b626f8b869481d03fec965457fdca
elliutCruz <erick.cruz@finaktiva.com>
2024-03-12 20:06:15 -0600

    [feat] pagina qr

--- src/app/ui/visitas/components/topbar/topbar.component.ts
+++ src/app/ui/visitas/components/topbar/topbar.component.ts
@@ -1,18 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { TopbarComponent } from '../../components/topbar/topbar.component';
 
 @Component({
-  selector: 'app-new-visit-page',
+  selector: 'app-topbar',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
-    TopbarComponent
+    RouterLink
   ],
-  templateUrl: './new-visit-page.component.html',
-  styleUrl: './new-visit-page.component.scss',
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewVisitPageComponent { }
+export class TopbarComponent { }
--- src/app/ui/visitas/pages/visit-qr-page/visit-qr-page.component.ts
+++ src/app/ui/visitas/pages/visit-qr-page/visit-qr-page.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TopbarComponent } from '../../components/topbar/topbar.component';
 
 @Component({
   selector: 'app-visit-qr-page',
   standalone: true,
   imports: [
     CommonModule,
+    TopbarComponent
   ],
   templateUrl: './visit-qr-page.component.html',
   styleUrl: './visit-qr-page.component.scss',
