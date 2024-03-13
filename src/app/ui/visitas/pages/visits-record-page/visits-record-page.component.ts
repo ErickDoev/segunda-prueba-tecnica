04f00d66d71c05876d74621ed585f5213afba173
elliutCruz <erick.cruz@finaktiva.com>
2024-03-12 18:08:40 -0600

    [feat] se agrega ruteo para agregar una nueva visita

--- src/app/ui/visitas/pages/visits-record-page/visits-record-page.component.ts
+++ src/app/ui/visitas/pages/visits-record-page/visits-record-page.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-visits-record-page',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink
   ],
   templateUrl: './visits-record-page.component.html',
   styleUrl: './visits-record-page.component.scss',
