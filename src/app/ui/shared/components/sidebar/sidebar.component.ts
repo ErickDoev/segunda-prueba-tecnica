95040daafb89ff9aaf25fbd9075872614083778d
elliutCruz <erick.cruz@finaktiva.com>
2024-03-20 13:28:49 -0600

    [fix] se termina de integrar componentes de Angular Material

--- src/app/ui/shared/components/sidebar/sidebar.component.ts
+++ src/app/ui/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [
     CommonModule,
+    MatListModule
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
--- src/app/ui/visitas/pages/visit-qr-page/visit-qr-page.component.ts
+++ src/app/ui/visitas/pages/visit-qr-page/visit-qr-page.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TopbarComponent } from '../../components/topbar/topbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-visit-qr-page',
   standalone: true,
   imports: [
     CommonModule,
-    TopbarComponent
+    TopbarComponent,
+    MatButtonModule,
+    RouterLink
   ],
   templateUrl: './visit-qr-page.component.html',
   styleUrl: './visit-qr-page.component.scss',
