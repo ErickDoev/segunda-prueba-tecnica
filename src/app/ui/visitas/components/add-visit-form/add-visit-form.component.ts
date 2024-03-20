38d2dc2b2a87bb2ee41df84a13307b2d48e68bea
elliutCruz <erick.cruz@finaktiva.com>
2024-03-20 12:51:06 -0600

    [fix] se aagregan componentes de angula material en la pagina de registrar visita

--- src/app/ui/visitas/components/add-visit-form/add-visit-form.component.ts
+++ src/app/ui/visitas/components/add-visit-form/add-visit-form.component.ts
@@ -1,16 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-visit-form',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    MatInputModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
   templateUrl: './add-visit-form.component.html',
   styleUrl: './add-visit-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddVisitFormComponent { }
+export class AddVisitFormComponent {
+    options: string[] = ['Madre', 'Padre'];
+    tolerancy: string[] = ['5 min', '10 min'];
+}
